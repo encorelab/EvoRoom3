97cd1c29021e4526cf188a72e56eaa50d5eddaab
Matt Zukowski <matt@zukowski.ca>
2012-02-08 20:51:59 -0500

    revised statemachine

--- agents/model/student_statemachine.rb
+++ agents/model/student_statemachine.rb
@@ -1,7 +1,7 @@
 # $: << '../../EvoRoom3/agents/sail.rb/lib'; $: << '../../EvoRoom3/agents'; require 'model/student'; Student.site = "http://rollcall.proto.encorelab.org"; s = Student.find('mzukowski'); load 'golem/visualizer.rb'; v = Golem::Visualizer.new(s.statemachine); v.visualize(:png, '../../EvoRoom3/agents/model/student_statemachine.png'); puts "--[ callbacks in use ]--"; s.statemachine.inspect.scan(/@callback=\:([\w_\?\!]+)/i).flatten.uniq.each{|m| puts (s.respond_to?(m) ? "√" : "x") + " #{m}"}; puts "--[ events ]--"; puts s.statemachine.events.values.collect{|ev| ev.name}; nil
 
 StudentStatemachine = proc do
-  initial_state :LOGGED_IN
+  initial_state :OUTSIDE
 
   state_attribute_writer (proc do |student, new_state|
     student.metadata.state = new_state
@@ -18,54 +18,61 @@ StudentStatemachine = proc do
 
   # DAY 1
 
-  state :LOGGED_IN do
+  state :OUTSIDE do
     on :check_in do
-      transition :to => :IN_ROOM do
+      transition :to => :ORIENTATION do
         guard(:failure_message => "the student must check in at the room entrance first") {|student, loc| loc == "room"}
       end
     end
   end
 
-  state :IN_ROOM do
-    on :rotation_start, :to => :IN_ROTATION,
-      :action => :increment_rotation!
-  end
-
-  state :IN_ROTATION do
-    enter :assign_rotation_location!
-    on :location_assignment, :to => :GOING_TO_ASSIGNED_LOCATION do
-      action do |student, loc|
-        student.metadata.currently_assigned_location = loc
-        student.metadata.going_to = 'observe_in_rotation'
+  state :ORIENTATION do
+    on :start do
+      transition :to => :WAITING_FOR_LOCATION_ASSIGNMENT do
+        guard :in_day_1?
+        action do |student|
+          student.metadata.current_task = "observe_past_presence"
+          student.increment_rotation!
+          student.assign_next_observation_location!
+        end
+      end
+      transition :to => :OBSERVING_PAST_FEATURES do
+        guard :in_day_2?
+        action do |student|
+          student.metadata.current_task = "observe_past_features"
+        end
       end
     end
   end
 
-  state :OBSERVING_IN_ROTATION do
-    on :organism_observation do
-      transition :to => :ROTATION_COMPLETED, :if => :rotation_completed?, :action => :store_observation
-      transition :to => :IN_ROTATION, :action => :store_observation
-    end
-  end
-  
-  state :ROTATION_COMPLETED do
-    enter do |student|
-      student.join_meetup!
-    end
+  state :WAITING_FOR_LOCATION_ASSIGNMENT do
     on :location_assignment, :to => :GOING_TO_ASSIGNED_LOCATION do
       action do |student, loc|
         student.metadata.currently_assigned_location = loc
-        student.metadata.going_to = 'meetup'
       end
     end
+  end
+
+  state :OBSERVING_PAST_PRESENCE do
+    exit do |student|
+      if student.observed_all_locations?
+        student.metadata.current_task = 'meetup'
+        student.assign_meetup_location!
+      else
+        student.assign_next_observation_location!
+      end
+    end
+    
+    on :organism_observation, :to => :WAITING_FOR_LOCATION_ASSIGNMENT, :action => :store_observation
   end
   
   state :GOING_TO_ASSIGNED_LOCATION do
     on :check_in do
       guard :at_assigned_location?, :failure_message => "the student is at the wrong location"
-      transition :to => :WAITING_FOR_MEETUP_TOPIC, :if => proc{|student| student.going_to?('meetup')}
-      transition :to => :OBSERVING_IN_ROTATION, :if => proc{|student| student.going_to?('observe_in_rotation')}
-      transition :to => :OBSERVING_PRESENT, :if => proc{|student| student.going_to?('observe_present')}
+      transition :to => :WAITING_FOR_MEETUP_TOPIC, :if => proc{|student| student.metadata.current_task == 'meetup'}
+      transition :to => :OBSERVING_PAST_PRESENCE, :if => proc{|student| student.metadata.current_task == 'observe_past_presence'}
+      transition :to => :OBSERVING_PRESENT_PRESENCE, :if => proc{|student| student.metadata.current_task == 'observe_present_presence'}
+      transition :to => :BRAINSTORMING, :if => proc{|student| student.metadata.current_task == 'brainstorm'}
     end
     # on :check_in do
     #   transition :action => proc{|student| student.agent.event!(:at_wrong_location) }
@@ -73,59 +80,66 @@ StudentStatemachine = proc do
   end
   
   state :WAITING_FOR_MEETUP_TOPIC do
-    on :topic_assignment, :to => :IN_MEETUP, 
+    on :topic_assignment, :to => :MEETUP, 
       :action => :store_meetup_topic
   end
   
-  state :IN_MEETUP do
+  state :MEETUP do
     on :note, :to => :WAITING_FOR_MEETUP_END, :action => :store_note
-    on :meetup_end, :to => :IN_ROTATION, :action => :increment_rotation!
-    on :homework_assignment, :to => :COMPLETED_DAY_1
+    on :meetup_end, :to => :WAITING_FOR_LOCATION_ASSIGNMENT do
+      action do |student|
+        student.metadata.current_task = "observe_past_presence"        
+        student.increment_rotation!
+        student.assign_next_observation_location!
+      end
+    end
+    on :homework_assignment, :to => :OUTSIDE do
+      action do |student|
+        student.metadata.day_1_completed = true
+      end
+    end
   end
   
   state :WAITING_FOR_MEETUP_END do
-    on :meetup_end, :to => :IN_ROTATION, :action => :increment_rotation!
-  end
-
-  # DAY 2
-
-  state :COMPLETED_DAY_1 do
-    on :check_in do
-      transition :to => :PAST do
-        guard(:failure_message => "the student must check in at the room entrance first") {|student, loc| loc == "room"}
+    on :meetup_end, :to => :WAITING_FOR_LOCATION_ASSIGNMENT do
+      action do |student|
+        student.metadata.current_task = "observe_past_presence"        
+        student.increment_rotation!
+        student.assign_next_observation_location!
+      end
+    end
+    on :homework_assignment, :to => :OUTSIDE do
+      action do |student|
+        student.metadata.day_1_completed = true
       end
     end
   end
   
-  state :PAST do
-    on :note, :action => :store_note
-    on :transition, :to => :PRESENT
-  end
-  
-  state :PRESENT do
-    enter :assign_present_location!
-    on :location_assignment, :to => :GOING_TO_ASSIGNED_LOCATION do
-      action do |student, loc|
-        student.metadata.currently_assigned_location = loc
-        student.metadata.going_to = 'observe_present'
+  state :OBSERVING_PAST_FEATURES do
+    on :organism_features, :action => :store_observation
+    on :transition_to_present, :to => :WAITING_FOR_LOCATION_ASSIGNMENT do
+      action do |student|
+        student.metadata.current_task = 'observe_present_presence'
+        student.assign_next_observation_location!
       end
     end
   end
   
-  state :OBSERVING_PRESENT do
-    on :observation_tabulation do
-      transition :to => :BRAINSTORMING do
-        guard :present_observations_completed?
-        action :store_note
-      end
-      transition :to => :PRESENT do
-        action :store_note
+  state :OBSERVING_PRESENT_PRESENCE do
+    exit do |student|
+      if student.observed_all_locations?
+        student.metadata.current_task = 'brainstorm'
+        student.assign_brainstorm_location!
+      else
+        student.assign_next_observation_location!
       end
     end
+    
+    on :observation_tabulation, :to => :WAITING_FOR_LOCATION_ASSIGNMENT, :action => :store_tabulation
   end
   
   state :BRAINSTORMING do
     on :concept_discussion, :action => :store_note
-    on :homework_assignment, :to => :COMPLETED_DAY_2
+    on :homework_assignment, :to => :DONE
   end
 end
