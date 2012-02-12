aa703f29b7720971d60897a9f5307308cff4d410
Matt Zukowski <matt@zukowski.ca>
2012-02-12 16:19:40 -0500

    fixed loading problems

--- agents/daemon.rb
+++ agents/daemon.rb
@@ -1,9 +1,6 @@
 $: << File.dirname(__FILE__)+"/sail.rb/lib"
+$: << File.dirname(__FILE__)
 require 'sail/daemon'
-
-require 'event_logger'
-require 'location_tracker'
-require 'choreographer'
 
 AGENT_PASSWORD = "9186ebc4790dfba833826e13c42c885f6f847274" # s3agent!
 
@@ -16,6 +13,11 @@ RUNS = ['a','b','c','d'].collect{|alph| "michelle-feb-2012-#{alph}"}
 )
 
 @daemon.load_config("../config.json")
+ENV['ROLLCALL_URL'] = @daemon.config[:rollcall][:url]
+
+require 'event_logger'
+require 'location_tracker'
+require 'choreographer'
 
 RUNS.each do |run|
   @daemon << EventLogger.new(:room => run, :password => AGENT_PASSWORD, :database => run)
