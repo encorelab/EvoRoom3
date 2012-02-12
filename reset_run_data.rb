e558fabfed4051be7a8f00e2a19f425eee6c0b7d
Matt Zukowski <matt@zukowski.ca>
2012-02-13 01:37:15 +0800

    reset_run_data updated for EvoRoom3

--- reset_run_data.rb
+++ reset_run_data.rb
@@ -4,8 +4,7 @@ require 'json'
 
 $: << "agents/sail.rb/lib"
 
-DB = "evoroom"
-RUN = "michelle-feb-2012"
+RUNS = ['a','b','c','d'].collect{|alph| "michelle-feb-2012-#{alph}"}
 
 require 'sail/rollcall/user'
 require 'sail/rollcall/group'
@@ -13,39 +12,48 @@ require 'sail/rollcall/group'
 json = File.read("config.json")
 config = JSON.parse(json, :symbolize_names => true)
 
-mongo = Mongo::Connection.new.db(DB)
+RUNS.each do |run|
+  puts ">> Resetting #{run.inspect}..."
+  
+  mongo = Mongo::Connection.new.db(run)
 
-puts "Wiping data in mongo..."
-mongo.collection(:location_tracking).remove()
-mongo.collection(:interviews).remove()
-mongo.collection(:events).remove()
-mongo.collection(:rainforest_guesses).remove()
-mongo.collection(:organism_presence).remove()
+  puts "  >> Wiping data in mongo database #{run.inspect}..."
+  mongo.collection(:location_tracking).remove()
+  mongo.collection(:observations).remove()
+  mongo.collection(:notes).remove()
+  mongo.collection(:meetups).remove()
 
-keep_metadata_keys = ["assigned_organism_1", "assigned_organism_2", "key"]
+  keep_metadata_keys = ["assigned_organisms", "speciality", "key"]
 
-Rollcall::User.site = config[:rollcall][:url]
+  Rollcall::Base.user = "rollcall"
+  Rollcall::Base.password = "rollcall!"
+  Rollcall::Base.site = config[:rollcall][:url]
 
-users_url = "/runs/#{RUN}/users.xml"
-puts "Pulling users from #{users_url}"
-Rollcall::User.format = ActiveResource::Formats::XmlFormat
-Rollcall::User.find(:all, :from => users_url).each do |u|
-  puts "Wiping metadata for #{u.account.login.inspect}..."
-  (u.metadata.attributes.keys).each do |key|
-    unless keep_metadata_keys.include?(key)
-      puts " - #{key}"
-      u.metadata.send("#{key}=", nil)
-    end
-    begin
-      g = Rollcall::Group.find(u.groups.first.id)
-      if g
-        puts "Clearing metadata for group #{g.name}"
-        g.metadata.assigned_location_for_guess = nil
-        g.save
+  users_url = "/runs/#{run}/users.xml"
+  full_url = Rollcall::Base.site.to_s + users_url
+  puts "  >> Pulling users from #{full_url.inspect}..."
+  
+  Rollcall::User.format = :xml
+  Rollcall::User.find(:all, :from => users_url).each do |u|
+    puts "  >> Wiping metadata for #{u.account.login.inspect}..."
+    (u.metadata.attributes.keys).each do |key|
+      unless keep_metadata_keys.include?(key)
+        puts "     - #{key}"
+        u.metadata.send("#{key}=", nil)
       end
-    rescue NoMethodError
-      puts "Not clearing group metadata for #{u.account.login.inspect}..."
+      # begin
+      #   g = Rollcall::Group.find(u.groups.first.id)
+      #   if g
+      #     puts "Clearing metadata for group #{g.name}"
+      #     g.metadata.assigned_location_for_guess = nil
+      #     g.save
+      #   end
+      # rescue NoMethodError
+      #   puts "Not clearing group metadata for #{u.account.login.inspect}..."
+      # end
     end
+    u.save
   end
-  u.save
+  
+  puts
 end
