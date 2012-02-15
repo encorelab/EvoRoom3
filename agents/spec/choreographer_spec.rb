39e75340b9a5ac56312ecbcdda951bf141997c53
Matt Zukowski <matt@zukowski.ca>
2012-02-15 12:37:33 -0500

    fixed current_locations to look for correct task name

--- agents/spec/choreographer_spec.rb
+++ agents/spec/choreographer_spec.rb
@@ -29,147 +29,147 @@ describe Choreographer do
   end
   
   it "should go through DAY 1 as per sequence diagram" do
-      @spec.config[:nickname] = "bobby"
-      
-      @spec.class_eval do
-        def behaviour
-          default_behaviour
-          
-          someone_joined_room do |stanza|
-            if Sail::Agent::Util.extract_login(stanza.from) == "Choreographer"
-              sequence do
-                ev(:check_in, {:location => "room"})
-                wait('ORIENTATION')
-                expect_student{ self.state.should == :ORIENTATION }
-                ev(:observations_start, {:rotation => 1}, {:origin => 'ateacher'})
-                wait('WAITING_FOR_LOCATION_ASSIGNMENT')
-                expect_student{ self.current_locations.length.should == 4 }
-                expect_student{ self.observed_locations_in_current_rotation.should == [] }
-                expect_student{ self.metadata.current_rotation.should == 1 }
-                
-                # ROTATION I
-                4.times do
-                  expect_student{ self.observed_all_locations?.should == false }
-                  on(:location_assignment){|stanza,data| @assigned_location = data['payload']['location'] }
-                  wait('GOING_TO_ASSIGNED_LOCATION')
-                  ev(:check_in, lambda{{:location => @assigned_location}})
-                  wait('OBSERVING_PAST')
-                  ev(:organism_observation, 
-                    lambda{{"location" => @assigned_location, "team_name" => "Darwin","assigned_organism" => "fig_tree","observed_organism" => "first","time" => "200 mya"}})
-                  wait('OBSERVING_PAST')
-                  ev(:organism_observation, 
-                    lambda{{"location" => @assigned_location, "team_name" => "Darwin","assigned_organism" => "fig_tree","observed_organism" => "second","time" => "200 mya"}})
-                  wait('OBSERVING_PAST')
-                  
-                  ev(:organism_observations_done, {})
-                  
-                  expect_student{ self.metadata.current_rotation.should == 1 }
-                  
-                  wait('WAITING_FOR_LOCATION_ASSIGNMENT')
-                end
-                
-                expect_student{ self.observed_all_locations?.should == true }
-                
-                # MEETUP I
-                on(:location_assignment){|stanza,data| @assigned_location = data['payload']['location'] }              
-                wait('GOING_TO_ASSIGNED_LOCATION')
-                
-                expect_student{ self.metadata.current_task.should == 'meetup'}
-                
-                meddle do
-                  stu = chor.lookup_student(config[:nickname])
-                  stu.stub(:team_is_assembled?).and_return(true)
-                end
-                
-                ev(:check_in, lambda{{:location => @assigned_location}})
-                wait('WAITING_FOR_MEETUP_START')
-                
-                #ev(:meetup_start, {}, {:origin => 'choreographer'})
-               
-                wait("MEETUP")
-                
-                ev(:note, {
-                  "author" => "JoeMcStudent",
-                  "team_name" => "Darwin",
-                  "meetup" => "1",
-                  "note" => "Blah blah blah...",
-                  "specialty" => "primates"
-                })
-                
-                wait("WAITING_FOR_GROUP_TO_FINISH_MEETUP")
-                ev(:observations_start, {}, {:origin => 'ateacher'})
-                wait('WAITING_FOR_LOCATION_ASSIGNMENT')
-                expect_student{ self.current_locations.length.should === 4 }
-                expect_student{ self.metadata.current_rotation.should == 2 }
-                
-                # ROTATION II
-                4.times do
-                  on(:location_assignment){|stanza,data| @assigned_location = data['payload']['location'] }
-                  wait('GOING_TO_ASSIGNED_LOCATION')
-                  ev(:check_in, lambda{{:location => @assigned_location}})
-                  wait('OBSERVING_PAST')
-                  
-                  ev(:organism_observation, 
-                    lambda{{"location" => @assigned_location, "team_name" => "Darwin","assigned_organism" => "fig_tree","observed_organism" => "fig_tree","time" => "200 mya"}})
-                  wait('OBSERVING_PAST')
-                  ev(:organism_observation, 
-                    lambda{{"location" => @assigned_location, "team_name" => "Darwin","assigned_organism" => "fig_tree","observed_organism" => "monkey","time" => "200 mya"}})
-                  wait('OBSERVING_PAST')
-                  ev(:organism_observations_done, {})
-                  
-                  expect_student{ self.metadata.current_rotation.should == 2 }
-                  
-                  wait('WAITING_FOR_LOCATION_ASSIGNMENT')
-                end
-                
-                # MEETUP II
+    @spec.config[:nickname] = "bobby"
+    
+    @spec.class_eval do
+      def behaviour
+        default_behaviour
+        
+        someone_joined_room do |stanza|
+          if Sail::Agent::Util.extract_login(stanza.from) == "Choreographer"
+            sequence do
+              ev(:check_in, {:location => "room"})
+              wait('ORIENTATION')
+              expect_student{ self.state.should == :ORIENTATION }
+              ev(:observations_start, {:rotation => 1}, {:origin => 'ateacher'})
+              wait('WAITING_FOR_LOCATION_ASSIGNMENT')
+              expect_student{ self.current_locations.length.should == 4 }
+              expect_student{ self.observed_locations_in_current_rotation.should == [] }
+              expect_student{ self.metadata.current_rotation.should == 1 }
+              
+              # ROTATION I
+              4.times do
+                expect_student{ self.observed_all_locations?.should == false }
                 on(:location_assignment){|stanza,data| @assigned_location = data['payload']['location'] }
                 wait('GOING_TO_ASSIGNED_LOCATION')
-                expect_student{ self.metadata.current_task.should == 'meetup'}
                 ev(:check_in, lambda{{:location => @assigned_location}})
-                wait('WAITING_FOR_MEETUP_START')
-  
-                #ev(:meetup_start, {}, {:origin => 'ateacher'})
-                wait("MEETUP")
-  
-                ev(:note, {
-                  "author" => "JoeMcStudent",
-                  "team_name" => "Darwin",
-                  "meetup" => "2",
-                  "note" => "Blah blah blah...",
-                  "specialty" => "primates"
-                })
-  
-                wait("WAITING_FOR_GROUP_TO_FINISH_MEETUP")
-                ev(:homework_assignment, {}, {:origin => 'ateacher'})
+                wait('OBSERVING_PAST')
+                ev(:organism_observation, 
+                  lambda{{"location" => @assigned_location, "team_name" => "Darwin","assigned_organism" => "fig_tree","observed_organism" => "first","time" => "200 mya"}})
+                wait('OBSERVING_PAST')
+                ev(:organism_observation, 
+                  lambda{{"location" => @assigned_location, "team_name" => "Darwin","assigned_organism" => "fig_tree","observed_organism" => "second","time" => "200 mya"}})
+                wait('OBSERVING_PAST')
                 
-                wait("OUTSIDE")
+                ev(:organism_observations_done, {})
                 
-                expect_student do
-                  # verify that expected metadata has been set 
-                  self.metadata.current_rotation.should === 2
-                  self.metadata.current_meetup.should === 2
-                  self.metadata.state.should === 'OUTSIDE'
-                  self.metadata.currently_assigned_location.blank?.should === false
-                  self.metadata.current_location.blank?.should === false
-                  self.metadata.day.should === 2
-                end
+                expect_student{ self.metadata.current_rotation.should == 1 }
                 
-                finish
+                wait('WAITING_FOR_LOCATION_ASSIGNMENT')
               end
+              
+              expect_student{ self.observed_all_locations?.should == true }
+              
+              # MEETUP I
+              on(:location_assignment){|stanza,data| @assigned_location = data['payload']['location'] }              
+              wait('GOING_TO_ASSIGNED_LOCATION')
+              
+              expect_student{ self.metadata.current_task.should == 'meetup'}
+              
+              meddle do
+                stu = chor.lookup_student(config[:nickname])
+                stu.stub(:team_is_assembled?).and_return(true)
+              end
+              
+              ev(:check_in, lambda{{:location => @assigned_location}})
+              wait('WAITING_FOR_MEETUP_START')
+              
+              #ev(:meetup_start, {}, {:origin => 'choreographer'})
+             
+              wait("MEETUP")
+              
+              ev(:note, {
+                "author" => "JoeMcStudent",
+                "team_name" => "Darwin",
+                "meetup" => "1",
+                "note" => "Blah blah blah...",
+                "specialty" => "primates"
+              })
+              
+              wait("WAITING_FOR_GROUP_TO_FINISH_MEETUP")
+              ev(:observations_start, {}, {:origin => 'ateacher'})
+              wait('WAITING_FOR_LOCATION_ASSIGNMENT')
+              expect_student{ self.current_locations.length.should === 4 }
+              expect_student{ self.metadata.current_rotation.should == 2 }
+              
+              # ROTATION II
+              4.times do
+                on(:location_assignment){|stanza,data| @assigned_location = data['payload']['location'] }
+                wait('GOING_TO_ASSIGNED_LOCATION')
+                ev(:check_in, lambda{{:location => @assigned_location}})
+                wait('OBSERVING_PAST')
+                
+                ev(:organism_observation, 
+                  lambda{{"location" => @assigned_location, "team_name" => "Darwin","assigned_organism" => "fig_tree","observed_organism" => "fig_tree","time" => "200 mya"}})
+                wait('OBSERVING_PAST')
+                ev(:organism_observation, 
+                  lambda{{"location" => @assigned_location, "team_name" => "Darwin","assigned_organism" => "fig_tree","observed_organism" => "monkey","time" => "200 mya"}})
+                wait('OBSERVING_PAST')
+                ev(:organism_observations_done, {})
+                
+                expect_student{ self.metadata.current_rotation.should == 2 }
+                
+                wait('WAITING_FOR_LOCATION_ASSIGNMENT')
+              end
+              
+              # MEETUP II
+              on(:location_assignment){|stanza,data| @assigned_location = data['payload']['location'] }
+              wait('GOING_TO_ASSIGNED_LOCATION')
+              expect_student{ self.metadata.current_task.should == 'meetup'}
+              ev(:check_in, lambda{{:location => @assigned_location}})
+              wait('WAITING_FOR_MEETUP_START')
+  
+              #ev(:meetup_start, {}, {:origin => 'ateacher'})
+              wait("MEETUP")
+  
+              ev(:note, {
+                "author" => "JoeMcStudent",
+                "team_name" => "Darwin",
+                "meetup" => "2",
+                "note" => "Blah blah blah...",
+                "specialty" => "primates"
+              })
+  
+              wait("WAITING_FOR_GROUP_TO_FINISH_MEETUP")
+              ev(:homework_assignment, {}, {:origin => 'ateacher'})
+              
+              wait("OUTSIDE")
+              
+              expect_student do
+                # verify that expected metadata has been set 
+                self.metadata.current_rotation.should === 2
+                self.metadata.current_meetup.should === 2
+                self.metadata.state.should === 'OUTSIDE'
+                self.metadata.currently_assigned_location.blank?.should === false
+                self.metadata.current_location.blank?.should === false
+                self.metadata.day.should === 2
+              end
+              
+              finish
             end
           end
         end
       end
-      
-      @chor.spawn!
-      @spec.spawn!
-      
-      EM.run {
-        @chor.run
-        @spec.run
-      }
     end
+    
+    @chor.spawn!
+    @spec.spawn!
+    
+    EM.run {
+      @chor.run
+      @spec.run
+    }
+  end
   
   it "should go through DAY 2 as per sequence diagram" do
     @spec.chor.class_eval do
@@ -227,7 +227,7 @@ describe Choreographer do
               expect_student{ self.metadata.current_rotation.should == 3 }
               
               # ROTATION III
-              4.times do
+              2.times do
                 expect_student{ self.observed_all_locations?.should == false }
                 on(:location_assignment){|stanza,data| @assigned_location = data['payload']['location'] }
                 wait('GOING_TO_ASSIGNED_LOCATION')
@@ -254,7 +254,7 @@ describe Choreographer do
               on(:location_assignment){|stanza,data| @assigned_location = data['payload']['location'] }
               wait('GOING_TO_ASSIGNED_LOCATION')
               
-              
+              expect_student{ self.metadata.current_task.should == 'brainstorm' }
               expect_student{ Student::LOCATIONS[:brainstorm].include?(self.metadata.currently_assigned_location).should == true }
               
               ev(:check_in, lambda{{:location => @assigned_location}})
