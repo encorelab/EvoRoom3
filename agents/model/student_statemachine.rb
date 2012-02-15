9ee23bcfc8e210f29883cc6d6d9538e419aa3168
Matt Zukowski <matt@zukowski.ca>
2012-02-15 17:43:07 -0500

    trying to fixed observed_all_locations? transition

--- agents/model/student_statemachine.rb
+++ agents/model/student_statemachine.rb
@@ -92,7 +92,7 @@ StudentStatemachine = proc do
       end
       transition :to => :WAITING_FOR_MEETUP_START do
         guard :failure_message => "student's current task is not 'meetup'" do |student|
-          student.metadata.current_task == 'observe_past_presence'
+          student.metadata.current_task == 'observe_past_presence' && student.observed_all_locations?
         end
       end
       transition :to => :OBSERVING_PAST do
@@ -107,7 +107,7 @@ StudentStatemachine = proc do
       end
       transition :to => :BRAINSTORMING do
         guard :failure_message => "student's current task is not 'brainstorm'" do |student|
-         student.metadata.current_task == 'brainstorm'
+         student.metadata.current_task == 'brainstorm' && student.observed_all_locations?
         end
       end
     end
